b0d96d7b3c0934407dea216adf15167b6324c770
rizalsuryana <rizalsuryana.dev@gmail.com>
2026-01-02 11:24:14 +0700

    [Setup] Project

---
+++ src/main/java/zays/rsuniversity/RsuniversityApplication.java
@@ -0,0 +1,13 @@
+package zays.rsuniversity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsuniversityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsuniversityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zays/rsuniversity/RsuniversityApplicationTests.java
@@ -0,0 +1,13 @@
+package zays.rsuniversity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsuniversityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
